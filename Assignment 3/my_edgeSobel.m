39c280e732fff7bf95acbade0171e9a0286459e1
Branden E. Smith <smith.brandene@gmail.com>
2015-02-18 11:12:26 -0600

    Computer-Vision-#4 Add logic to get the edgeMagnitude image.
    Added the logic to apply a threshold to the sobel filter so that the
    algorithm only adds pixel values who's gradient is above the threshold
    to the final image.

--- Assignment 3/my_edgeSobel.m
+++ Assignment 3/my_edgeSobel.m
@@ -23,6 +23,36 @@ verticalOperator = [1 2 1; 0 0 0; -1 -2 -1];
 horizontalGradient = imfilter(image, horizontalOperator);
 verticalGradient = imfilter(image, verticalOperator);
 
-edgeMagnitude = horizontalGradient + verticalGradient;
+unThresholdedMagnitude = horizontalGradient + verticalGradient;
+[height, width, channels] = size(unThresholdedMagnitude);
+
+edgeMagnitude = zeros(height, width, channels);
+
+unThresholdedRed = unThresholdedMagnitude(:,:,1);
+unThresholdedGreen = unThresholdedMagnitude(:,:,2);
+unThresholdedBlue = unThresholdedMagnitude(:,:,3);
+
+thresholdedRed = threshold(unThresholdedRed, THRESHOLD_VAL);
+thresholdedGreen = threshold(unThresholdedGreen, THRESHOLD_VAL);
+thresholdedBlue = threshold(unThresholdedBlue, THRESHOLD_VAL);
+
+edgeMagnitude(:,:, 1) = thresholdedRed;
+edgeMagnitude(:,:, 2) = thresholdedGreen;
+edgeMagnitude(:,:, 3) = thresholdedBlue;
+
+    function thresholdedChannel = threshold(image, THRESHOLD_VAL)
+        [x,y,z] = size(image);
+        thresholdedChannel = zeros(x,y,z);
+        
+        for i=1:x
+            for j=1:y
+                if (image(i,j) >= THRESHOLD_VAL)
+                    thresholdedChannel(i,j) = image(i,j);
+                end
+            end
+        end
+    end
+
+edgeOrientation = zeros();
 end
 
