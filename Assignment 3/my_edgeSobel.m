9798bda1200ae4c5554f173ae56c48c4a2cc8150
Branden E. Smith <smith.brandene@gmail.com>
2015-02-18 10:21:11 -0600

    Computer-Vision-#4 Add logic to use imfilter function.

--- Assignment 3/my_edgeSobel.m
+++ Assignment 3/my_edgeSobel.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % my_edgeSobel.m
-% 
+%
 % Author Branden E. Smith & Mathew Reny
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -8,13 +8,23 @@
 function [edgeMagnitued, edgeOrientation] = my_edgeSobel(image, THRESHOLD_VAL)
 
 % Partial derivatives (discrete) with respect to x and y.
-partialX = image(x+1, y) - image(x,y);
-partialY = image(x, y+1) - image(x,y);
+% partialX = image(x+1, y) - image(x,y);
+% partialY = image(x, y+1) - image(x,y);
+
+
 
 % The two sobel operators that are commonly used
-% the first horizontalOperator shold be scaled by the partial with 
+% the first horizontalOperator shold be scaled by the partial with
 % respect to x and the verticalOperator should be scaled with respect to
 % y
 horizontalOperator = [-1 0 1; -2 0 2; -1 0 1];
 verticalOperator = [1 2 1; 0 0 0; -1 -2 -1];
+
+horizontalGradient = imfilter(image, horizontalOperator);
+verticalGradient = imfilter(image, verticalOperator);
+
+figure(1); imshow(horizontalGradient);
+figure(2); imshow(verticalGradient);
+
+end
 
