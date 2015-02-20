a7121905b61a2c09e26ed4b42b8f323020c5cd34
Branden E. Smith <smith.brandene@gmail.com>
2015-02-19 21:42:05 -0600

    Computer-Vision-#7 Create base for Marr-Hildreth algorithm.
    Created the basic class for the Marr-Hildreth operator. Currently,
    The Gaussian filter is computed manually by the algorithm. The function
    computes the filter size based on the input parameter sigma and creates
    a Gaussian filter of that size. The function uses the Gaussian filter
    to blur the image and then computes the second order derivative of the
    blurred image.

---
+++ Assignment 3/my_edgeMarrHildreth.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% my_edgeMarrHildreth.m
+% 
+% Author: Branden E. Smith & Mathew Reny
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function edgeMagnitude = my_edgeMarrHildreth(image, sigma)
+
+% Determine filter size N = [sigma x 3] x 2 + 1
+N = (sigma * 3)*2 + 1;
+
+% Differential Operators
+partialX = [0 0 0; 0 -1 1; 0 0 0];
+partialY = [0 0 0; 0 -1 0; 0 1 0];
+
+% Gaussian filter of size N as shown on the slide in class.
+G = double(zeros(N));
+
+for i=1:N
+    for j=1:N
+        G(i,j) = exp(-(i^2 + j^2)/(2*pi*sigma));
+    end
+end
+
+% Step one
+iPrime = imfilter(image, G);
+
+% Take the first Derivative of iPrime
+iPrimeFirstDerivX = imfilter(iPrime, partialX);
+iPrimeFirstDerivY = imfilter(iPrime, partialY);
+
+% Take the second Derivative of iPrime
+iPrimeSecondDerivX = imfilter(iPrimeFirstDerivX, partialX);
+iPrimeSecondDerivY = imfilter(iPrimeFirstDerivY, partialY);
+
+iDoublePrime = iPrimeSecondDerivX + iPrimeSecondDerivY;
+
+edgeMagnitude = iDoublePrime;
+
+end
