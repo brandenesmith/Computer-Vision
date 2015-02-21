2ca582526273cb2a30638484cecc08ba2ed33501
Branden E. Smith <smith.brandene@gmail.com>
2015-02-20 22:20:16 -0600

    Computer-Vision-#7 Fix zero crossings.
    Fixed the zero crossings portion of the algorithm. In its current state,
    the edgeMagnitude image is created as all white (1) and then for
    every pixel in the second derivative (Laplacian of the Gaussian in this
    case) if the neighbor to the left, right, top, or bottom is black then
    the pixel becomes black. This change signifies where the
    second order derivative changed from positive to negative or made a
    zero crossing.

--- Assignment 3/my_edgeMarrHildreth.m
+++ Assignment 3/my_edgeMarrHildreth.m
@@ -9,29 +9,27 @@ function edgeMagnitude = my_edgeMarrHildreth(image, sigma)
 % Determine filter size N = [sigma x 3] x 2 + 1 and the
 % size of the image.
 N = ceil(sigma * 3)*2 + 1;
-[height, width, channels] = size(image);
+[height, width, ~] = size(image);
 
+% Use the laplacian of the gaussian.
 log = fspecial('log', N, sigma);
-conv = double(imfilter(image, log));
+convolved = double(imfilter(image, log));
 
 % Threshold the image
-conv = im2bw(conv, 0);
+convolved = im2bw(convolved, 0);
 
-padded = padarray(conv, [1,1]);
-edgeMagnitude = zeros(height, width, channels);
+padded = padarray(convolved, [1,1], 1);
+edgeMagnitude = ones(height, width, 1);
 
 % Zero Crossings
 for i=1:height
     for j=1:width
-        if (padded(i+1, j+1) == 0)
-            A = padded(i:(i+2) , j:(j+2));
-            colSum = sum(A);
-            totSum = sum(colSum);
-            if (totSum == 0)
-                edgeMagnitude(i,j) = 1;
+        if (padded(i+1, j+1) == 1)
+            if(padded(i,j+1) == 0 || padded(i+1, j+2) == 0 ||...
+                    padded(i+2, j+1) == 0 || padded(i+1, j) == 0)
+                edgeMagnitude(i,j) = 0;
             end
         end
     end
 end
-
 end
