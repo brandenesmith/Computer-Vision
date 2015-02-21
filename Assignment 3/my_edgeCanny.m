59053bc50a47e1f58f64389081f447315c5deb01
Branden E. Smith <smith.brandene@gmail.com>
2015-02-20 23:46:48 -0600

    Computer-Vision-#8 Compute gradient using the derivative of gaussian.
    Added logic to compute the gradient using the first derivative of the
    gaussian filtered image. Also, added logic to compose the gradient
    magnitude and orientation. The gradient magnitude is thresholded on
    the threshold parameter L.

--- Assignment 3/my_edgeCanny.m
+++ Assignment 3/my_edgeCanny.m
@@ -6,4 +6,43 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function canny = my_edgeCanny(image, L, H, sigma)
 
+% Setup
+[height, width, channels] = size(image);
+
+% Filter image with the x,y derivatives of the Gaussian
+gaussian = fspecial('Gaussian', [5, 5], sigma);
+gaussianImage = imfilter(image, gaussian);
+
+partialX = [-1 1];
+partialY = [-1; 1];
+
+derivOfGaussianX = double(imfilter(gaussianImage, partialX));
+derivOfGaussianY = double(imfilter(gaussianImage, partialY));
+
+% Find magnitude and orientation of gradient
+xSquared = derivOfGaussianX.^2;
+ySquared = derivOfGaussianY.^2;
+
+gradientMagnitude = double(sqrt(xSquared + ySquared));
+gradientOrientation = double(atan(derivOfGaussianY./derivOfGaussianX));
+
+% Threshold magnitude at min value
+for c=1:channels
+    for i=1:height
+        for j=1:width
+            if (gradientMagnitude(i,j,c) < L)
+                gradientMagnitude(i,j,c) = 0;
+            end
+        end
+    end
+end
+
+% Non-maximum suppression (Thin multi-pixel "ridges" down to single pixel
+% width
+
+% Hysteresis thresholding and linking
+% -Define two thresholds: low and high
+% -Use the high threshold to start edge curves and the
+%  low to continue them
+canny = gaussianImage;
 end
