b2b0ce1a9f651fe216edccabc3abd7a89de065df
Branden E. Smith <smith.brandene@gmail.com>
2015-03-04 10:00:41 -0600

    Computer-Vision-#19 Create get_interest_points.m script.
    Created the skeleton of the get_interest_points.m that will contain
    the implementation of Harris corner detection.

---
+++ Assignment 4/get_interest_points.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% get_interest_points.m
+%
+% Author: Branden E. Smith
+%
+% Finds interesting points (corners) in an image and returns them.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function points = get_interest_points(image)
+end
