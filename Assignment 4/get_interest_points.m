39332e7192bcb5f58dea33638bc237f5792fe2f2
Branden E. Smith <smith.brandene@gmail.com>
2015-03-04 10:41:25 -0600

    Computer-Vision-#19 Add logic to blur the image and compute derivatives.
    - Added logic to blur the image using a gaussian blur and then compute
    the partial derivatives with respect to x and y of the blurred image.
    
    - Also, added logic to square both of the partials of x and y for the
    next portion of the scripts functionality.

--- Assignment 4/get_interest_points.m
+++ Assignment 4/get_interest_points.m
@@ -6,4 +6,42 @@
 % Finds interesting points (corners) in an image and returns them.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function points = get_interest_points(image)
+% Sigma for Gaussian Blur
+SIGMA = 7;
+
+% Image size information (if needed).
+[x, y, z] = size(image);
+
+% Compute M matrix for each image window to get their cornerness
+% scores.
+
+% (Optionally) blur the image and compute the 
+% partial derivatives of the image with respect 
+% to x and y. This can be tweeked to provide
+% varying results. 
+filter = fspecial('Gaussian', 7, SIGMA);
+image = imfilter(image, filter);
+
+% Partial derivative filters
+partialX = [-1 1];
+partialY = [-1; 1];
+
+% Filter the image with partial derivative filters.
+partialX = imfilter(image, partialX);
+partialY = imfilter(image, partialY);
+
+% Square the partial derivative images.
+partial_x_squared = partialX.*partialX;
+partial_y_squared = partialY.*partialY;
+
+% Convert Partials to Gray
+partial_x_squared = rgb2gray(partial_x_squared);
+partial_y_squared = rgb2gray(partial_y_squared);
+
+
+% Find points whose surrounding window gave large corner response
+% i.e. (f > threshold).
+
+% Take the points of local maxima, i.e., perform non-maximum suppression.
+
 end
