1a29285d990bc80dcaaca77ebce2fe53166c9cd9
Branden E. Smith <smith.brandene@gmail.com>
2015-03-04 18:28:47 -0600

    Computer-Vision-#19 Add variable to get rid of magic number.
    - Added the variable alpha to get rid of the magic number .04
    - Restructured the code so that the comments correspond to their
    appropriate lines of code.

--- Assignment 4/get_interest_points.m
+++ Assignment 4/get_interest_points.m
@@ -8,6 +8,7 @@
 function points = get_interest_points(image)
 % Sigma for Gaussian Blur
 SIGMA = 1;
+alpha = .04;
 
 % Image size information (if needed).
 [x, y, z] = size(image);
@@ -15,7 +16,6 @@ SIGMA = 1;
 %%
 % Compute the horizontal and vertical derivatives of the image by
 % convolving the original image with derivatives of Guassians.
-
 filter = fspecial('Gaussian', 7, SIGMA);
 image = double(imfilter(image, filter));
 
@@ -27,32 +27,24 @@ d_y = transpose(d_x);
 I_x = imfilter(image, d_x);
 I_y = imfilter(image, d_y);
 
-% Square the partial derivative images.
+%%
+% Compute the three images corresponding to the outer products of these
+% gradients. (The matrix is symmetric, so only three entries are needed.
 I_xx = I_x.*I_x;
 I_yy = I_y.*I_y;
 I_xy = I_x.*I_y;
 
+%%
+% Convolve each of these images with a larger Gaussian.
 I_xxg = imfilter(I_xx, filter);
 I_yyg = imfilter(I_yy, filter);
 I_xyg = imfilter(I_xy, filter);
-
-% Multiply both images
-response = ((I_xxg.*I_yyg)-(I_xyg.*I_xyg)) - .04*(I_xxg + I_yyg).^2;
-
-
-
-%%
-% Compute the three images corresponding to the outer products of these
-% gradients. (The matrix is symmetric, so only three entries are needed.
-
-
-%%
-% Convolve each of these images with a larger Gaussian.
 
 %%
 % Compute a scalar interest measure. i.e. Find points whose surrounding 
 % window gave large corner response
 % i.e. (f > threshold).
+response = ((I_xxg.*I_yyg)-(I_xyg.*I_xyg)) - alpha*(I_xxg + I_yyg).^2;
 
 %%
 % Find local maxima above a certain threshold and report them as detected
